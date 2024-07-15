1b3d929e2d8429049be81cbe3e8d03899d757076
Divviti <divviti.a.tejaswani@capgemini.com>
2024-07-15 12:07:59 +0530

    django_first_sample_webpage

---
+++ my_site/challenges/views.py
@@ -0,0 +1,74 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect,Http404
+from django.urls import reverse
+from django.template.loader import render_to_string
+
+monthly_challenges = {
+    "january": "Eat no meat for the entire month!",
+    "february": "Walk for at least 20 minutes every day!",
+    "march": "Learn Django for at least 20 minutes every day!",
+    "april": "Eat no meat for the entire month!",
+    "may": "Walk for at least 20 minutes every day!",
+    "june": "Learn Django for at least 20 minutes every day!",
+    "july": "Eat no meat for the entire month!",
+    "august": "Walk for at least 20 minutes every day!",
+    "september": "Learn Django for at least 20 minutes every day!",
+    "october": "Eat no meat for the entire month!",
+    "november": "Walk for at least 20 minutes every day!",
+    "december": None
+}
+
+# Create your views here.
+
+def index(request):
+    # list_items = ""
+    months = list(monthly_challenges.keys())
+
+    # for month in months:
+    #     capitalized_month = month.capitalize()
+    #     month_path = reverse("month-challenge", args=[month])
+    #     list_items += f"<li><a href=\"{month_path}\">{capitalized_month}</a></li>"
+
+    # # "<li><a href="...">January</a></li><li><a href="...">February</a></li>..."
+
+    # response_data = f"<ul>{list_items}</ul>"
+    # return HttpResponse(response_data)
+    return render(request,"challenges/index.html",{
+        "months":months     # this for dynamic url generation 
+    })
+
+def monthly_challenge_by_number(request, month):
+    months = list(monthly_challenges.keys())
+
+    if month > len(months):
+        return HttpResponseNotFound("Invalid month")
+
+    redirect_month = months[month - 1]
+    redirect_path = reverse("month-challenge", args=[redirect_month]) # /challenge/january
+    return HttpResponseRedirect(redirect_path)
+
+
+def monthly_challenge(request, month):
+    # try:
+    #     challenge_text = monthly_challenges[month]
+    #     response_data = render_to_string("challenges/challenge.html")
+    #     return HttpResponse(response_data)
+    # except:
+    #     return HttpResponseNotFound("<h1>This month is not supported!</h1>")
+    try:
+        text = monthly_challenges[month]
+
+        return render(request,"challenges/challenge.html",{
+            "text":text,
+            "month_name":month
+        })
+    except:
+        # return HttpResponseNotFound("<h1>This month is not supported!</h1>")
+        #  response_data=render_to_string("404.html")   #here we did'nt add relavtive path bcoz it is in base tempaltes folder
+        #  return HttpResponseNotFound(response_data)
+        raise Http404()
+     #Http404  this is class we can use directly raise Http404()  we no need to pass anything but in root tempalte folder that 404.html should be there
+#HttpResponseNotFound if we are using this we are responsible for generating html reponses by our own 
+#instaed of this we can generate dynamic 404.html 
+
+
